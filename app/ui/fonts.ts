48db51f150def9e5423f131497bc3f96e1dfd135
Tom <tsloan82@gmail.com>
2024-04-22 19:17:21 -0500

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,11 @@
-import {Inter, Andika } from 'next/font/google';
+import {Inter, Andika, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    style:['normal'], 
+    subsets: ['latin'],
+    weight:['400']
+});
 export const andika = Andika({
     style:['normal'], 
     subsets: ['cyrillic'],
