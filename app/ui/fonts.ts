6eeb001df82e198e5511e57824dc0b0707e27de0
Tom <tsloan82@gmail.com>
2024-04-22 19:10:30 -0500

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Inter, Andika } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const andika = Andika({
+    style:['normal'], 
+    subsets: ['cyrillic'],
+    weight:['400']
+});
